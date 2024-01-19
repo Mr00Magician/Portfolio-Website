7426faf5e6a4eee27d2a77dde6d05fdcc0d80832
Mr00Magician <meanasnadeem@gmail.com>
2024-01-19 23:27:11 +0530

    style: add animation to hide "insights" behind "data"
    - Add animation to introductory text in hero section.
    - show the word "insights" when mouse hovers over the word "data"

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,11 +4,25 @@ import heroBackground from "../assets/images/hero section background.png"
 import portfolioImg from "../assets/images/portfolio pic upscaled.png"
 
 export default function Hero(){
+
+    function showInsights() {
+        document.getElementById("insights").style.animation = "show-insights 1s ease both";
+    }
+
+    function hideInsights() {
+        document.getElementById("insights").style.animation = "hide-insights 1s ease both";
+    }
+
     return (
         <div className = "hero">
             <img className = "hero-background" src = {heroBackground} alt = "hero background"/>
             <img className = "portfolio-img" src = {portfolioImg} alt = "Anas' Image"/>
-            <h1 className = "intro-txt">Hi! I Am Anas,<br/>A Data Scientist.</h1>
+            <div className = "intro-txt">
+                Hi! I Am Anas,<br/>
+                A <div id = "data" onMouseEnter={showInsights} onMouseLeave={hideInsights}>
+                    Data <div id = "insights-wrapper"><div id = "insights">Insights</div></div>
+                </div> Scientist
+            </div>
         </div>
     )
 }
