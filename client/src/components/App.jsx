88e32aa73b330ef365101d08d0d39790fee4ca68
Mr00Magician <meanasnadeem@gmail.com>
2024-01-26 23:02:29 +0530

    Update `App` to render `Tile` component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css"
 import Hero from "./Hero";
+import Tile from "./Tile";
 
 export default function App() {
     return (
-        <Hero />
+        <Tile child={Hero} height={"60vh"}/>
     )
 }
